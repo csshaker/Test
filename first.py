2ff30aded548c9ce96ea94319e2b23265c3bbde3
Md. Shaker Ahmed <shaker.ahmed@bjitgroup.com>
2020-03-09 18:39:52 +0600

    deleted

--- first.py
+++ first.py
@@ -1,14 +1,4 @@
 def print_something(something):
 	print(something)
 
-def function_office_test():
-	print("office test")
-def new_function():
-	print("new function")
-
-def func_master():
-	print("master")
-
-def function_office_master():
-	print("office master")
 
